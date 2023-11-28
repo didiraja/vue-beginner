f1190e18ce8ee5e82f82ed382a1ed9faf9af58b6
Dico Didiraja <didiraja@hotmail.com>
2023-11-28 11:55:21 -0300

    feat: 🎸 finished lesson #4

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{title}}</h1>
+  <Modal />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue App',
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
+  },
 }
 </script>
 
@@ -22,5 +31,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 5px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
